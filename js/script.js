c166a61e9cc6be03ff19a63f3e6ae5bcb8facf0a
Frank Rauen <home@Franks-MacBook-Air.local>
2019-11-23 15:33:10 -0700

    adds HTML, JS, and CSS

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ $('form').on('submit', (event) =>{
     event.preventDefault();
     const userInput = $('input[type = "text"]').val();
     $.ajax({
-        url: 'api.openweathermap.org/data/2.5/weather?q=' + userInput
-    })
+        url: 'http://api.openweathermap.org/data/2.5/weather?appid=748c648c1a83be10f1608e4048797989&q=' + userInput +
+        '&units=imperial'
+    }).then(
+        (data) => {
+            console.log(data);
+            $('#city-name').html(data.name);
+            $('#description').html(data.weather[0].description);
+            $('#temperature').html(data.main.temp + ' Farenheit');
+            $('#high').html(data.main.temp_max + ' Farenheit');
+            $('#low').html(data.main.temp_min + ' Farenheit');
+            $('#humidity').html(data.main.humidity + ' Percent');
+            $('#wind-speed').html(data.wind.speed + ' Knots');
+        },
+        (error) => {
+            console.log('Error: ', error);
+        });
 });
